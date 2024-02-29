29da07b42b168c898b7083e459acabaf98d85ed4
Pavlo Safonov <psp@sitecore.net>
2024-02-29 23:03:26 +0200

    added scripts changed config added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+## Installation
+
+- `npm i` to install all
+
+## How to run tests?
+
+- `npx playwright test` - Running all tests
+- `npx playwright test selectFacet.spec.ts` - Running a single test file
+
+## Useful links
+
+- `https://playwright.dev/` - Playwright-test documentation
+- `https://trace.playwright.dev/` - Trace viewer
+
+## Used tools
+
+- [microsoft/playwright](https://github.com/microsoft/playwright) - Playwright-test repo
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
     baseURL: "https://www.douglas.de/de",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
@@ -37,13 +38,13 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
